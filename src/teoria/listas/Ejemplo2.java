89c6fc1139c1a3e7273979f0209cc2ef2772bdc5
manuel <mmolmil158@g.educaand.es>
2023-12-15 11:14:20 +0100

    Fin List en teoría

--- src/teoria/listas/Ejemplo2.java
+++ src/teoria/listas/Ejemplo2.java
@@ -18,8 +18,33 @@ public class Ejemplo2 {
         System.out.println("=====ELIMINANDO EL 3====");
         eliminarNumero(enteros, 3);
         System.out.println(enteros);
+        System.out.println("=====AÑADIR EL 100====");
+        addNumero(enteros, 100);
+        System.out.println(enteros);
+        System.out.println("=====AÑADIR EL -1 A POSICION 0====");
+        addNumeroEnIndex(enteros, -1, 0);
+        System.out.println(enteros);
+        System.out.println("=====AÑADIR EL -100 A POSICION 100====");
+        addNumeroEnIndex(enteros, -100, 100);
+        System.out.println(enteros);
+        System.out.println("====¿CONTIENE EL 5?==========");
+        System.out.println(contieneNumero(enteros, 5));
+        System.out.println("=====ACTUALIZARMOS LA PRIMERA POSICIÓN A 10======");
+        actualizar(enteros, 10, 0);
+        System.out.println(enteros);
+        System.out.println("=====ACTUALIZARMOS LA  POSICIÓN 100 A 10======");
+        actualizar(enteros, 10, 100);
+        System.out.println(enteros);
+
 
     }
+
+    private static void actualizar(List<Integer> enteros, Integer numero, int index) {
+        if (index > enteros.size() -1)
+            return;
+        enteros.set(index, numero);
+    }
+
 
     private static List<Integer> crearListaTamanno(int tamanno) {
         List<Integer> listaPares = new ArrayList<>();
@@ -42,13 +67,28 @@ public class Ejemplo2 {
 
     //método que se le pasa una colección de enteros y un  número
     //y elimina eL primer número
-    public static void eliminarNumero(List<Integer> enteros, Integer numero) {
+    private static void eliminarNumero(List<Integer> enteros, Integer numero) {
+
         enteros.remove(numero);
     }
 
     //método que se le pasa una colección de enteros y un entero
     //y añade este entero a la colección
+    private static void addNumero(List<Integer> enteros, int numero) {
+        enteros.add(numero);
+    }
 
     //método que se le pasa una colección de enteros, un entero,
     //un índice y añade el entero a esa posición índice
+    private static void addNumeroEnIndex(List<Integer> enteros, int numero, int index) {
+        if (index > enteros.size() - 1)
+            return;
+        enteros.add(index, numero);
+    }
+    //método que se le pasa una colección de enteros, un entero y nos dice si contiene
+    //ese valor
+    private static boolean contieneNumero(List<Integer> enteros, Integer numero) {
+        return enteros.contains(numero);
+    }
+
 }
